696b6eb5bd055bcd36d1d633c0097ead1ac1df34
kastre1474 <81590817+kastre1474@users.noreply.github.com>
2022-02-26 11:40:06 -0600

    Add files via upload

---
+++ DBConnect.py
@@ -0,0 +1,13 @@
+import pyodbc 
+ 
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=ymca.database.windows.net;'
+                      'Database=ymca;'
+                      'Trusted_Connection=yes;PWD=AdminUser2022!;UID=ymca;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM dbo.[User]')
+
+for i in cursor:
+    print(i)
---
+++ Driver.py
@@ -0,0 +1,11 @@
+ 
+import os
+import Window
+
+def main():
+
+    Window.createPage()
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(__file__))
+    main()  
