9dca2c2129d25c299f03b769b89a740d8a32c5e3
Benjamin Waldo <bwaldo1127@gmail.com>
2022-02-26 12:43:41 -0600

    updating win

--- init_window.py
+++ init_window.py
@@ -3,61 +3,42 @@ from tkinter.font import Font
 import os
 import pyodbc
 
-conn = None
-window = None
-mode = None
-user = -1
 
-def exitFullScreen(event=None):
-    global window
-    window.attributes("-fullscreen", False)
+class initwin():
 
-def enterFullScreen(event=None):
-    global window
-    window.attributes("-fullscreen", True)
+    def __init__(self, root):
+        self.win = root
+        self.setup()
+        self.buttonOptions()
     
-def clearWindow():
-    global window
-    for widget in window.winfo_children():
-        widget.destroy()
-
-def setupWindow():
-    global window
-    window.configure(background = 'white')
-    window.bind("<Escape>", exitFullScreen)
-    window.bind("<F1>", enterFullScreen)
-    window.attributes("-fullscreen", True)
-    screenWidth = window.winfo_screenwidth()
-    screenHeight = window.winfo_screenheight()
-    canvas = tk.Canvas(window, width= screenWidth, height= screenHeight / 10, bg='cyan')
-    canvas.create_text(screenWidth / 2, screenHeight / 20, text="Welcome To The YMCA", fill="Black", font=Font(family = "Helvetica", size = 30, weight = "bold"))
-    canvas.grid(row = 0, column = 0, columnspan = 11)
-    buttonOptions()
-
-def buttonOptions(i = -1):
-    if i == -1:
-        b1 = tk.Button(window, text = 'Home', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
-        b2 = tk.Button(window, text = 'Programs', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
-        b3 = tk.Button(window, text = 'About', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
-        b4 = tk.Button(window, text = 'Login/SignUp', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
+    def setup(self):
+        self.win.configure(background = 'white')
+        self.win.bind("<Escape>", self.exitFullScreen)
+        self.win.bind("<F1>", self.enterFullScreen)
+        self.win.attributes("-fullscreen", True)
+        screenWidth = self.win.winfo_screenwidth()
+        screenHeight = self.win.winfo_screenheight()
+        canvas = tk.Canvas(self.win, width= screenWidth, height= screenHeight / 10, bg='cyan')
+        canvas.create_text(screenWidth / 2, screenHeight / 20, text="Welcome To The YMCA", fill="Black", font=Font(family = "Helvetica", size = 30, weight = "bold"))
+        canvas.grid(row = 0, column = 0, columnspan = 11)
+        
+    def buttonOptions(self):
+        b1 = tk.Button(self.win, text = 'Home', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
+        b2 = tk.Button(self.win, text = 'Programs', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
+        b3 = tk.Button(self.win, text = 'About', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
+        b4 = tk.Button(self.win, text = 'Login/SignUp', command = None, font = Font(family = "Helvetica", size = 20, weight = "bold"))
         b1.grid(row = 1, column = 0, columnspan = 1, sticky = tk.W+tk.E, pady = 10)
         b2.grid(row = 2, column = 0, columnspan = 1, sticky = tk.W+tk.E, pady = 10)
         b3.grid(row = 3, column = 0, columnspan = 1, sticky = tk.W+tk.E, pady = 10)
         b4.grid(row = 4, column = 0, columnspan = 1, sticky = tk.W+tk.E, pady = 10)
-    elif i == 0:
-        pass
-    elif i == 1:
-        pass
-    else:
-        pass
-    
+        
+    def exitFullScreen(self, event=None):
+        self.win.attributes("-fullscreen", False)
 
-def createPage():
+    def enterFullScreen(self, event=None):
+        self.win.attributes("-fullscreen", True)
+        
 
-    global window
-    window = tk.Tk()
-    setupWindow()
-    window.mainloop()
  
 
 
