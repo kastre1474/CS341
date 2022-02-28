8aeb599a596a4687d5bb0426fca0a73d872fc955
BenjaminWaldo <82007324+BenjaminWaldo@users.noreply.github.com>
2022-02-28 09:57:34 -0600

    Delete Conn.py

--- Conn.py
+++
@@ -1,8 +0,0 @@
-import pyodbc 
-
-class conn():
-    def __init__(self):
-        self.conn = pyodbc.connect('Driver={SQL Server Native Client 11.0};Server=ymca.database.windows.net;Database=ymca;Trusted_Connection=no;PWD=AdminUser2022!;UID=ymca;')
-        self.cursor = self.conn.cursor()
-        
-
