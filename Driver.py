bd3f964e8fb040d3b50424394ef5f9949bff48dd
Your NameTrevor <you@example.com>
2022-02-28 09:49:35 -0600

    Updating Driver

--- Driver.py
+++ Driver.py
@@ -1,11 +1,10 @@
-import tkinter as tk
+
 import os
 import init_window
 
 def main():
-    master = tk.Tk()
-    init_window.initwin(master)
-    master.mainloop()
+
+    init_window.createPage()
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
